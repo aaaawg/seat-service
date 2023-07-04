526e4f7b7fac4c54e86b953b21889e5059e42f60
leeyan821 <mu1521@naver.com>
2023-07-04 14:30:52 +0900

    로그인 후 세션에 30분 유지... 세션으로 정보 불러오는 건 아직 X

--- src/main/java/com/psr/seatservice/controller/user/UserController.java
+++ src/main/java/com/psr/seatservice/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package com.psr.seatservice.controller.user;
 
+import com.psr.seatservice.SessionConst;
 import com.psr.seatservice.domian.user.User;
 import com.psr.seatservice.dto.user.request.AddBizUserRequest;
 import com.psr.seatservice.dto.user.request.UserLoginRequest;
@@ -7,6 +8,9 @@ import com.psr.seatservice.service.user.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -23,12 +27,31 @@ public class UserController {
 
     //로그인
     @PostMapping("/login")
-    public String login(UserLoginRequest request) {
+    public String login(UserLoginRequest request, HttpServletRequest httpServletRequest) {
         User loginUser = userService.login(request);
-        if(loginUser != null && loginUser.getRole().equals("biz"))
+        if(loginUser != null && loginUser.getRole().equals("biz")) {
+            HttpSession session = httpServletRequest.getSession();
+            session.setAttribute(SessionConst.LOGIN_MEMBER, loginUser);
+            System.out.println("idid: " + SessionConst.LOGIN_MEMBER);
+
+            session.getAttributeNames().asIterator()
+                    .forEachRemaining(name -> System.out.println("session name=" + name + "value=" + session.getAttribute(name)));
             return "program/bizProgramList";
-        else
+        }
+        else {
+            System.out.println("noidid: " + SessionConst.LOGIN_MEMBER);
             return "program/programList";
+        }
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        //세션을 삭제한다.
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "program/programList";
     }
 
     @GetMapping("/join")
