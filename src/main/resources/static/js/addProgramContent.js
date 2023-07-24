5cbf6b63126d5efbab69552e0dc9913f7b87e456
leeyan821 <mu1521@naver.com>
2023-07-25 03:50:09 +0900

    프론트 수정 중

--- src/main/resources/static/js/addProgramContent.js
+++ src/main/resources/static/js/addProgramContent.js
@@ -1,10 +1,20 @@
 let count = 0;
+
+const newType = {
+short: "text",
+long: "text",
+selectOne: "radio",
+selectMore: "checkbox"
+};
+const getType = (selectType) => {return newType[selectType];};
+console.log(getType("short"));
+
 function addContent(){
     const element = document.getElementById('content');
     // new div 생성
     const newContentDiv = document.createElement('div');
     newContentDiv.setAttribute("id", "plus"+count)
-    newContentDiv.style.background = 'yellow';
+    newContentDiv.style.background = '#fffce2';
     newContentDiv.style.padding = "20px 0 20px 0";
 
     addButtons(newContentDiv);
@@ -12,14 +22,35 @@ function addContent(){
     element.appendChild(newContentDiv);
 }
 
+function makeButton(className, idName, clickName){
+    var newBtn = document.createElement('button');
+    newBtn.setAttribute("type", "button");
+    newBtn.setAttribute("class", className);
+    newBtn.setAttribute("id", idName);
+    newBtn.setAttribute("onclick", clickName);
+    return newBtn;
+}
+
+function makeTextInput(className, placeholderName){
+    var input = document.createElement('input');
+    input.setAttribute("type", "text");
+    input.setAttribute("class", className);
+    input.setAttribute("placeholder", placeholderName);
+    return input;
+}
+
+function makeRadio(){
+
+}
+
+function makeCheckBox(){
+
+}
+
 //편집, 지우기 버튼 생성
 function addButtons(newContentDiv){
         //edit button
-        var newBtn = document.createElement( 'button' );
-        newBtn.setAttribute("type", "button");
-        newBtn.setAttribute("class", "btn btn-change");
-        newBtn.setAttribute("id", count);
-        newBtn.setAttribute("onclick", "addEditSelect(this.id)");
+        var newBtn = makeButton("btn btn-change", count, "addEditSelect(this.id)");
 
         var newI = document.createElement('i');
         newI.setAttribute("class","bi bi-pencil-square");
@@ -28,11 +59,7 @@ function addButtons(newContentDiv){
         newContentDiv.appendChild(newBtn);
 
         //delete button
-        var newDelBtn = document.createElement( 'button' );
-        newDelBtn.setAttribute("type", "button");
-        newDelBtn.setAttribute("class", "btn btn-delete");
-        newDelBtn.setAttribute("id", count);
-        newDelBtn.setAttribute("onclick", "deleteSelect(this.id)");
+        var newDelBtn = makeButton("btn btn-delete", count, "deleteSelect(this.id)");
 
         var newDelI = document.createElement('i');
         newDelI.setAttribute("class","bi bi-trash3");
@@ -41,43 +68,41 @@ function addButtons(newContentDiv){
         newContentDiv.appendChild(newDelBtn);
 }
 
-//기본 주관식 단답형 생성
+//기본 제목, 설명란, 처음 생성시 짧은 주관식
 function origContent(newContentDiv){
-    // new div 생성
     const newDiv = document.createElement('div');
     newDiv.setAttribute("class", count)
 
     var span1 = document.createElement( 'span' );
     var titleText = document.createTextNode( '항목 제목' );
-    var input1 = document.createElement( 'input' );
-    // set attribute (input)
-	input1.setAttribute("type", "text");
-	input1.setAttribute("class", "sample"+count);
-	input1.setAttribute("placeholder", "제목 입력");
+    var input1 = makeTextInput("title"+count, "제목 입력");
 
     span1.appendChild( titleText );
     newDiv.appendChild(span1);
     newDiv.appendChild(input1);
 
-    count++;
-    // new div 생성
     const newDiv2 = document.createElement('div');
     newDiv2.setAttribute("class", count)
 
     var span2 = document.createElement( 'span' );
     var titleText2 = document.createTextNode( '항목 설명' );
-    var input2 = document.createElement( 'input' );
-    input2.setAttribute("type", "text");
-    input2.setAttribute("class", "sample"+count);
+    var input2 = makeTextInput("content"+count, "");
 
     span2.appendChild( titleText2 );
     newDiv2.appendChild(span2);
     newDiv2.appendChild(input2);
 
-    count++;
+    const newDiv3 = document.createElement('div');
+    newDiv3.setAttribute("class", "reDiv"+count);
 
+    var inputOrigShort = makeTextInput("reply"+count, "답변");
+    inputOrigShort.setAttribute('readonly','');
+    newDiv3.appendChild(inputOrigShort);
+
+    count++;
     newContentDiv.appendChild(newDiv);
     newContentDiv.appendChild(newDiv2);
+    newContentDiv.appendChild(newDiv3);
 }
 //편집 셀렉트 생성
 function addEditSelect(e){
@@ -85,12 +110,16 @@ function addEditSelect(e){
     const newDiv = document.createElement('div');
     newDiv.setAttribute("id","select"+e);
 
+    var span = document.createElement('span');
+    var TypeText = document.createTextNode('유형');
+    span.appendChild(TypeText);
+    newDiv.appendChild(span);
+
     var select = document.createElement( 'select' );
     select.setAttribute("name", "content"+e);
     select.setAttribute("onchange", "handleOnChange(this, "+e+")");
 
-    var con = ['주관식 단답형','주관식 서술형', '단일 선택형', '복수 선택형',
-     '목록 선택형', '선호도형', '날짜/시간', '연락처', '주소', '숫자', '파일'];
+    var con = ['주관식 단답형','주관식 서술형', '단일 선택형', '복수 선택형'];
 
     for(let i=0; i<con.length; i++) {
         var opt = document.createElement( 'option' );
@@ -101,11 +130,7 @@ function addEditSelect(e){
     newDiv.appendChild(select);
     divName.appendChild(newDiv);
 
-    var newChBtn = document.createElement( 'button' );
-    newChBtn.setAttribute("type", "button");
-    newChBtn.setAttribute("class", "btn btn-success");
-    newChBtn.setAttribute("id", "complete"+e);
-    newChBtn.setAttribute("onclick", "complete("+e+")");
+    var newChBtn = makeButton("btn btn-success", "complete"+e, "complete("+e+")");
     newChBtn.innerText = "완료";
     divName.appendChild(newChBtn);
 }
@@ -114,7 +139,23 @@ function handleOnChange(e, divNum) {
   const value = e.value;
 
   const divName = document.getElementById('plus'+divNum);
-   alert(value+ divNum);
+  alert(value+ divNum);
+  //변경 시 reDiv + divNum의 컨텐츠 변경해야함
+
+  switch(e.value){
+    case '주관식 단답형':
+        //
+        break;
+    case 주관식 서술형':
+        //
+        break;
+    case '단일 선택형':
+        //
+        break;
+    default :
+        //
+        break;
+  }
   //divName.textContent = "";
 
 }
