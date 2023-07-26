d95423cc4527ea6c741c4178c617acd01055ff37
leeyan821 <mu1521@naver.com>
2023-07-27 04:11:33 +0900

    radio 까지 완료

--- src/main/resources/static/js/addProgramContent.js
+++ src/main/resources/static/js/addProgramContent.js
@@ -36,15 +36,74 @@ function makeTextInput(className, placeholderName){
     input.setAttribute("type", "text");
     input.setAttribute("class", className);
     input.setAttribute("placeholder", placeholderName);
+    input.setAttribute('maxlength',10);
     return input;
 }
 
-function makeRadio(){
-
+function makeTextArea(className, placeholderName){
+    var input = document.createElement('textarea');
+    input.setAttribute("class", className);
+    input.setAttribute("placeholder", placeholderName);
+    input.setAttribute('disabled','');
+    return input;
 }
 
-function makeCheckBox(){
+function makeRadio(name, selectValue){
+//<label><input type="radio" name="fruit" value="apple"> Apple</label>
+    var label = document.createElement('label');
+    label.setAttribute("id", "select"+selectValue+name);
+    var inputRadio = document.createElement('input');
+    inputRadio.setAttribute("type", "radio");
+    inputRadio.setAttribute("name", name);
+    inputRadio.setAttribute("value", selectValue);
+    var inputText = document.createElement('input');
+    inputText.setAttribute("type", "text");
+    inputText.setAttribute("name", name+"_reason");
 
+    var deleteButton = makeButton("select"+selectValue+name, "delete"+selectValue, "clickRadioMinus(this.className)");
+    var newI = document.createElement('i');
+    newI.setAttribute("class","bi bi-x-square");
+    deleteButton.appendChild(newI);
+
+    label.appendChild(inputRadio);
+    label.appendChild(inputText);
+    label.appendChild(deleteButton);
+    return label;
+}
+function clickRadioPlus(divNum){
+    var selectList = document.getElementsByName('reply'+divNum);
+    var lastValue = selectList[selectList.length-1].value;
+    const changeContent = document.getElementsByClassName('selectDiv'+divNum);
+    var label = makeRadio("reply"+divNum, ++lastValue);
+    changeContent[0].appendChild(label);
+}
+
+function clickRadioMinus(deleteName){
+    const changeContent = document.getElementById(deleteName);
+    changeContent.remove();
+}
+
+function makeCheckBox(name, selectValue){
+//<input type="checkbox" name="colors" id="chk4" value="black"><label for="radio4">블랙</label>
+    var label = document.createElement('label');
+    label.setAttribute("id", "checkBox"+selectValue+name);
+    var inputCheckBox = document.createElement('input');
+    inputCheckBox.setAttribute("type", "checkbox");
+    inputCheckBox.setAttribute("name", name);
+    inputCheckBox.setAttribute("value", selectValue);
+    var inputText = document.createElement('input');
+    inputText.setAttribute("type", "text");
+    inputText.setAttribute("name", name+"_reason");
+
+    var deleteButton = makeButton("checkBox"+selectValue+name, "deleteCheckBox"+selectValue, "clickRadioMinus(this.className)");
+    var newI = document.createElement('i');
+    newI.setAttribute("class","bi bi-x-square");
+    deleteButton.appendChild(newI);
+
+    label.appendChild(inputCheckBox);
+    label.appendChild(inputText);
+    label.appendChild(deleteButton);
+    return label;
 }
 
 //편집, 지우기 버튼 생성
@@ -96,7 +155,7 @@ function origContent(newContentDiv){
     newDiv3.setAttribute("class", "reDiv"+count);
 
     var inputOrigShort = makeTextInput("reply"+count, "답변");
-    inputOrigShort.setAttribute('readonly','');
+    inputOrigShort.setAttribute('disabled','');
     newDiv3.appendChild(inputOrigShort);
 
     count++;
@@ -137,27 +196,43 @@ function addEditSelect(e){
 //셀렉트 선택시
 function handleOnChange(e, divNum) {
   const value = e.value;
-
   const divName = document.getElementById('plus'+divNum);
-  alert(value+ divNum);
-  //변경 시 reDiv + divNum의 컨텐츠 변경해야함
+
+  const changeContent = document.getElementsByClassName('reDiv'+divNum);
+  changeContent[0].innerText = '';
 
   switch(e.value){
     case '주관식 단답형':
-        //
+        var changeInputText = makeTextInput("reply"+divNum, "단답형 답변");
+        changeContent[0].appendChild(changeInputText);
         break;
-    case 주관식 서술형':
-        //
+    case '주관식 서술형':
+        var changeInputText = makeTextArea("reply"+divNum, "서술형 답변");
+        changeContent[0].appendChild(changeInputText);
         break;
     case '단일 선택형':
-        //
+        const wholeSelectDiv = document.createElement('div');
+        wholeSelectDiv.setAttribute("class", "wholeSelectDiv"+divNum);
+
+        const selectDiv = document.createElement('div');
+        selectDiv.setAttribute("class", "selectDiv"+divNum);
+        var label = makeRadio("reply"+divNum, 0);
+        selectDiv.appendChild(label);
+        wholeSelectDiv.appendChild(selectDiv);
+
+        const selectBtnDiv = document.createElement('div');
+        selectBtnDiv.setAttribute("class", "selectBtnDiv"+divNum);
+        var radioPlusBtn = makeButton("radioPlus"+divNum, "radioP"+divNum, "clickRadioPlus("+divNum+")");
+        radioPlusBtn.innerText = "옵션 추가";
+        selectBtnDiv.appendChild(radioPlusBtn);
+
+        wholeSelectDiv.appendChild(selectBtnDiv);
+        changeContent[0].appendChild(wholeSelectDiv);
         break;
     default :
         //
         break;
   }
-  //divName.textContent = "";
-
 }
 
 function deleteSelect(e){
