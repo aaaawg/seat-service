42f063bcfa15c085a607c1ad4557399e247f3ded
leeyan821 <mu1521@naver.com>
2023-07-28 01:30:00 +0900

    기본 추가 기능 완료 이벤트 기능 수정 시작

--- src/main/resources/static/js/addProgramContent.js
+++ src/main/resources/static/js/addProgramContent.js
@@ -1,17 +1,7 @@
 let count = 0;
 
-const newType = {
-short: "text",
-long: "text",
-selectOne: "radio",
-selectMore: "checkbox"
-};
-const getType = (selectType) => {return newType[selectType];};
-console.log(getType("short"));
-
 function addContent(){
     const element = document.getElementById('content');
-    // new div 생성
     const newContentDiv = document.createElement('div');
     newContentDiv.setAttribute("id", "plus"+count)
     newContentDiv.style.background = '#fffce2';
@@ -106,29 +96,34 @@ function makeCheckBox(name, selectValue){
     return label;
 }
 
+function clickCheckPlus(divNum){
+    var selectList = document.getElementsByName('replyCh'+divNum);
+    var lastValue = selectList[selectList.length-1].value;
+    const changeContent = document.getElementsByClassName('checkBoxDiv'+divNum);
+    var label = makeCheckBox("replyCh"+divNum, ++lastValue);
+    changeContent[0].appendChild(label);
+}
 //편집, 지우기 버튼 생성
 function addButtons(newContentDiv){
-        //edit button
         var newBtn = makeButton("btn btn-change", count, "addEditSelect(this.id)");
-
         var newI = document.createElement('i');
         newI.setAttribute("class","bi bi-pencil-square");
 
         newBtn.appendChild(newI);
         newContentDiv.appendChild(newBtn);
 
-        //delete button
         var newDelBtn = makeButton("btn btn-delete", count, "deleteSelect(this.id)");
-
         var newDelI = document.createElement('i');
         newDelI.setAttribute("class","bi bi-trash3");
 
         newDelBtn.appendChild(newDelI);
         newContentDiv.appendChild(newDelBtn);
 }
-
 //기본 제목, 설명란, 처음 생성시 짧은 주관식
 function origContent(newContentDiv){
+    const origContent = document.createElement('div');
+    origContent.setAttribute("id", "origContent"+count);
+
     const newDiv = document.createElement('div');
     newDiv.setAttribute("class", count)
 
@@ -139,6 +134,7 @@ function origContent(newContentDiv){
     span1.appendChild( titleText );
     newDiv.appendChild(span1);
     newDiv.appendChild(input1);
+    origContent.appendChild(newDiv);
 
     const newDiv2 = document.createElement('div');
     newDiv2.setAttribute("class", count)
@@ -150,6 +146,7 @@ function origContent(newContentDiv){
     span2.appendChild( titleText2 );
     newDiv2.appendChild(span2);
     newDiv2.appendChild(input2);
+    origContent.appendChild(newDiv2);
 
     const newDiv3 = document.createElement('div');
     newDiv3.setAttribute("class", "reDiv"+count);
@@ -159,12 +156,12 @@ function origContent(newContentDiv){
     newDiv3.appendChild(inputOrigShort);
 
     count++;
-    newContentDiv.appendChild(newDiv);
-    newContentDiv.appendChild(newDiv2);
+    newContentDiv.appendChild(origContent);
     newContentDiv.appendChild(newDiv3);
 }
 //편집 셀렉트 생성
 function addEditSelect(e){
+    titleInputLayoutShow(e);
     const divName = document.getElementById('plus'+e);
     const newDiv = document.createElement('div');
     newDiv.setAttribute("id","select"+e);
@@ -192,6 +189,11 @@ function addEditSelect(e){
     var newChBtn = makeButton("btn btn-success", "complete"+e, "complete("+e+")");
     newChBtn.innerText = "완료";
     divName.appendChild(newChBtn);
+
+    const target = document.getElementById(e);
+    if(target.disabled === false){
+        target.disabled = true;
+    }
 }
 //셀렉트 선택시
 function handleOnChange(e, divNum) {
@@ -229,8 +231,24 @@ function handleOnChange(e, divNum) {
         wholeSelectDiv.appendChild(selectBtnDiv);
         changeContent[0].appendChild(wholeSelectDiv);
         break;
-    default :
-        //
+    case '복수 선택형':
+        const wholeCheckDiv = document.createElement('div');
+        wholeCheckDiv.setAttribute("class", "wholeCheckDiv"+divNum);
+
+        const checkBoxDiv = document.createElement('div');
+        checkBoxDiv.setAttribute("class", "checkBoxDiv"+divNum);
+        var checkBoxLabel = makeCheckBox("replyCh"+divNum, 0);
+        checkBoxDiv.appendChild(checkBoxLabel);
+        wholeCheckDiv.appendChild(checkBoxDiv);
+
+        const checkBoxBtnDiv = document.createElement('div');
+        checkBoxBtnDiv.setAttribute("class", "checkBoxBtnDiv"+divNum);
+        var checkPlusBtn = makeButton("checkPlus"+divNum, "checkP"+divNum, "clickCheckPlus("+divNum+")");
+        checkPlusBtn.innerText = "옵션 추가";
+        checkBoxBtnDiv.appendChild(checkPlusBtn);
+
+        wholeCheckDiv.appendChild(checkBoxBtnDiv);
+        changeContent[0].appendChild(wholeCheckDiv);
         break;
   }
 }
@@ -244,4 +262,64 @@ function complete(e){
     selectName.remove();
     const buttonName = document.getElementById('complete'+e);
     buttonName.remove();
+    const target = document.getElementById(e);
+    if(target.disabled === true){
+       target.disabled = false;
+    }
+
+
+    titleResult(e);
+}
+
+function titleInputLayoutShow(num){
+    var layout = document.getElementById('titleConReplyDiv'+num);
+    var removeDiv = document.getElementsByClassName(num);
+    var length = removeDiv.length;
+    if(layout != null){
+        layout.style.display='none';
+        for(let i=0; i<length; i++)
+            removeDiv[i].style.display='block';
+    }
+}
+
+function titleResult(num){
+    const removeDiv = document.getElementsByClassName(num);
+    var length = removeDiv.length;
+    var title = document.getElementsByClassName("title"+num)[0].value;
+    var content = document.getElementsByClassName("content"+num)[0].value;
+    for(let i=0; i<length; i++)
+        removeDiv[i].style.display='none';
+
+    if(document.getElementById('titleConReplyDiv'+num) === null){
+        if(title==='')
+            title = '제목을 입력해주세요.';
+        const origContent = document.getElementById('origContent'+num);
+
+        const replyDiv = document.createElement('div');
+        replyDiv.setAttribute("id", "titleConReplyDiv"+num);
+
+        var spanTitle = document.createElement('span');
+        spanTitle.setAttribute("id", "titleSpan"+num);
+        var titleText = document.createTextNode(title);
+        spanTitle.style.fontSize = "20px";
+        var br = document.createElement('br');
+
+        var spanContent = document.createElement('span');
+        spanContent.setAttribute("id", "ContentSpan"+num);
+        var contentText = document.createTextNode(content);
+
+        spanTitle.appendChild(titleText);
+        spanContent.appendChild(contentText);
+
+        replyDiv.appendChild(spanTitle);
+        replyDiv.appendChild(br);
+        replyDiv.appendChild(spanContent);
+        origContent.appendChild(replyDiv);
+    }else{
+        alert('이미 있는 경우');
+        var result = document.getElementById('titleConReplyDiv'+num);
+        result.style.display='block';
+        var ChangeTitleSpan = document.getElementById("titleSpan"+num);
+        ChangeTitleSpan.innerText = title;
+    }
 }
